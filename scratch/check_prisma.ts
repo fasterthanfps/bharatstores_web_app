f9bdfe16434d583afadb24ef3efe07a5873b9467
fasterthanfps <akthecount@gmail.com>
2026-05-16 19:59:37 +0530

    chore: cleanup scratch files

--- scratch/check_prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-console.log('PrismaClient constructor keys:', Object.keys(PrismaClient.prototype));
-// This might not work if it's not instantiated.
-try {
-  const p = new PrismaClient({} as any);
-  console.log('Instance keys:', Object.keys(p));
-} catch(e) {}
