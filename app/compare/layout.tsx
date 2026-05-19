a77cd3f05a41b0e49188e3dcb286ba933774d2e3
fasterthanfps <akthecount@gmail.com>
2026-05-20 00:45:56 +0530

    fix: remove remaining MobileNav reference from compare layout

--- app/compare/layout.tsx
+++ app/compare/layout.tsx
@@ -1,16 +1,14 @@
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
-import MobileNav from '@/components/layout/MobileNav';
 
 export default function CompareLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Header />
-            <main className="min-h-screen pb-[60px] lg:pb-0">
+            <main className="min-h-screen">
                 {children}
             </main>
             <Footer />
-            <MobileNav />
         </>
     );
 }
